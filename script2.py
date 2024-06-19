41305cdbd4bf1e31dba5c4ad428a8131f805a8b4
unknown <Minskoleopc>
2024-06-19 08:31:33 +0530

    added

---
+++ script2.py
@@ -0,0 +1,96 @@
+# introvert    extrovert 
+# calm          loud
+# less outing   more outing
+# less social   more social
+
+# human --- type  --- amol
+# properties - color , gender , height , weight
+# methods - walk() , talk()
+
+# vehicle - type - car
+# properties - color , type , regNo , model
+# methods - start() , stop()
+
+# bank acc 
+# properties - accNo , bal , IFSC code , accName ,branchName
+# deposit() , withdrawl()
+
+a = 10
+print(a)
+print(type(a))
+#10,-10,0
+
+b = 10.5
+print(b)
+print(type(b))
+# 10.6,-10.8,0.23
+
+c = True 
+print(c)
+print(type(c))
+# True False
+
+d = "chinmay"
+print(d)
+print(type(d))
+"C","1","123#@!$#@"," "
+
+# comparison operator 
+
+# < , > ,<= , >= , == ,!=
+# entity < entity ------ o/p boolean ---- true or false
+
+print(7 > 3) # True
+print(7 < 3)  # False
+print(7 >= 3) # True
+print(7 <= 3) # False
+print(7 >= 7) # True
+print(7 <= 7) # True
+print(7 != 7) # False
+print(7 == 8) # False
+print(8 == 8) # True
+
+# Logical operator
+
+#and 
+
+# true   and   true  --- true
+# false  and   true  --- false
+# true   and   false --- false
+# false  and   false --- false 
+
+print(6 == 6 and 8 != 7)
+print(5 == 3 and 9 == 9)
+print(5 == 5 and 8 == 7 )
+print(8 == 7 and 5 == 4)
+
+#or 
+
+# true  or  true  ---- true 
+# false or  true  ---- true
+# true  or  false ---  true 
+# false or  false ---  false
+
+print(7 == 7 or 9 == 9)
+print(7 == 8 or 9 == 9)
+print(6 != 8 or 9 == 7)
+print(8 != 8 or 9 != 9)
+
+
+# not
+# not true  --- false 
+# not false --  true
+
+print(not (7 == 3))
+print(not (8 == 8))
+
+
+
+
+
+
+
+# logical operaor
+
+
+# conditional statements 
