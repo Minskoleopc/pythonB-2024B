41c7b33218e9b949699fb567eae5605faa984e1a
unknown <Minskoleopc>
2024-07-12 08:36:08 +0530

    added

--- script15.py
+++ script15.py
@@ -46,6 +46,8 @@ str = "123213123123"
 e8 = str.isdigit()
 print(e8)
 
+#isnum()
+
 str2 = "amol"
 e9 = str2.isalpha()
 print(e9)
@@ -68,6 +70,55 @@ print(str3.isalnum())
 str = "i am learning python"
 e10 = str.replace("python","js")
 print(e10)
+
+
+str2  = "Hello"
+e2 = str2.islower()
+print(e2)
+
+
+str3 = "HELLO"
+e4 = str3.isupper()
+print(e4)
+
+
+str4 = "chinmaya"
+e3 = str4.count('a')
+print(e3)
+
+str5 = "I Learn Python"
+e4 = str5.istitle()
+print(e4)
+
+str6 = "I Am Learning  Python"
+e5 = str6.swapcase()
+print(e5)
+
+#rjust()
+#ljust()
+
+# isspace()
+str7 = " "
+e8 = str7.isspace()
+print(e8)
+
+# varaibles -----> types ---- comparison ---- logical 
+# conditional statements ----> loops ----- list 
+# dictionary   strings  ----- tuple ----- set
+
+
+#7:30pm
+
+a = 10
+print(a)
+a = "hello"
+print(a)
+
+#int a = 12
+
+
+
+
 
 
 
